139ccf34c00f08fab91b1c5c33c5b13065fef6a1
shaked <shaked1hen@gmail.com>
2020-07-17 15:45:25 +0300

    added the curret models

--- elder_gurder_proj/elder_gurder_service/admin.py
+++ elder_gurder_proj/elder_gurder_service/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from .models import CustomUser , ToDoList
+from .models import CustomUser , LonelyPeople , Visitis
 
 
 # Register your models here.
 admin.site.register(CustomUser)
-admin.site.register(ToDoList)
+admin.site.register(Visitis)
+admin.site.register(LonelyPeople)
+
--- elder_gurder_proj/elder_gurder_service/models.py
+++ elder_gurder_proj/elder_gurder_service/models.py
@@ -7,8 +7,7 @@ from django.contrib.auth import get_user_model
 class CustomUser(AbstractUser):
     email = models.EmailField(verbose_name='email', max_length=255, unique=True)
     username = models.CharField(max_length=255)
-    password = models.CharField(max_length=255)
-    REQUIRED_FIELDS = ['username'] 
+    REQUIRED_FIELDS = [''] 
     USERNAME_FIELD = 'email'
 
     def get_username(self):
@@ -18,24 +17,37 @@ class CustomUser(AbstractUser):
         return self.email
 
 
-
+# lonely
 # Create your models here.
-class ToDoList(models.Model):
+class LonelyPeople(models.Model):
     user = models.ForeignKey(
         get_user_model(),
         verbose_name='user',
         on_delete=models.CASCADE)
-    header = models.CharField(max_length=150)
-    task = models.TextField(null=True)
+    name    = models.CharField(max_length=150)
+    age     = models.IntegerField()
+    address = models.CharField(max_length=150)
+    phone   = models.CharField(max_length=20)
+    deatils = models.TextField(null=True)
     
     def __str__(self):
-        return self.header
+        return f'The name is {self.name} and his address is {self.address}'
 
-class Item(models.Model):
-    ToDoList = models.ForeignKey(ToDoList, on_delete=models.CASCADE)
-    text = models.CharField(max_length=300)
-    complete = models.BooleanField()
-
-    def __str__(self):
-        return self.text
+class Visitis(models.Model):
+    user = models.ForeignKey(
+        get_user_model(),
+        verbose_name='user',
+        related_name="visitor",
+        on_delete=models.CASCADE)
+    loneny = models.ForeignKey(
+        LonelyPeople(),
+        verbose_name='loneny',
+        related_name="visited",
+        on_delete=models.CASCADE)
+    date = models.DateField(verbose_name='data')
+    visit_details = models.TextField(null=True)
     
+    def __str__(self):
+        return f'{self.user} visit {self.loneny} at {self.date}'
+
+
