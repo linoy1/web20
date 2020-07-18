c44b41edf5fa351195f086cbc47ec29a72947342
shaked <shaked1hen@gmail.com>
2020-07-18 15:03:09 +0300

    added the data href link

--- elder_gurder_proj/elder_gurder_service/static/JS/main.js
+++ elder_gurder_proj/elder_gurder_service/static/JS/main.js
@@ -1,33 +1,5 @@
-$(document).ready(function() {
-    $('#example').DataTable({
-        "dom": '<"toolbar">frtip'
+jQuery(document).ready(function($) {
+    $('*[data-href]').on('click', function() {
+        window.location = $(this).data("href");
     });
-
-    $("div.toolbar").html('<b>Custom tool bar! Text/images etc.</b>');
-});
-/*people page */
-
-
-$(document).ready(function() {
-    $('#dtBasicExample').DataTable({
-        "paging": false // false to disable pagination (or any other option)
-    });
-    $('.dataTables_length').addClass('bs-select');
-});
-
-
-$(document).ready(function() {
-    $('#memployboxes-1').change(function() {
-        $('#motherdiv').fadeIn();
-    });
-    $('#memployboxes-0').change(function() {
-        $('#motherdiv').fadeOut();
-    });
-    $('#femployboxes-1').change(function() {
-        $('#fatherdiv').fadeIn();
-    });
-    $('#femployboxes-0').change(function() {
-        $('#fatherdiv').fadeOut();
-    });
-
 });
